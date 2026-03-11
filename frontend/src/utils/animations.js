ecf453f80b42eb6a3a802b8f3a2ee724db998839
01rohitsingh <rohitgidha2000@gmail.com>
2026-03-11 17:22:59 +0530

    complete

--- frontend/src/utils/animations.js
+++ frontend/src/utils/animations.js
@@ -4,8 +4,8 @@ export const cardAnimation = {
   initial: { opacity: 0, y: 12 },
   animate: { opacity: 1, y: 0 },
 
-  whileHover: { y: -3, scale: 1.01 },
-  // whileTap: { scale: 0.97 },
+  whileHover: { y: -3, scale: 1.01 }, // PC hover
+  whileTap: { scale: 0.97 }, // Mobile tap
 
   transition: {
     type: "tween",
@@ -15,8 +15,8 @@ export const cardAnimation = {
 };
 
 export const iconAnimation = {
-  whileHover: { scale: 1.08 },
-  // whileTap: { scale: 0.92 },
+  whileHover: { scale: 1.08 }, // PC hover
+  whileTap: { scale: 0.92 }, // Mobile tap
 
   transition: {
     type: "tween",
@@ -25,8 +25,8 @@ export const iconAnimation = {
 };
 
 export const buttonAnimation = {
-  whileHover: { scale: 1.03 },
-  whileTap: { scale: 0.92 },
+  whileHover: { scale: 1.05 }, // PC hover
+  whileTap: { scale: 0.92 }, // Mobile tap
 
   transition: {
     type: "tween",
